8d9076ea3cbea83f26c32193acdf6e74ff4fd4e7
SirJimKe <jimmymutuku1@gmail.com>
2022-08-26 13:37:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i <= array.length; i++){
+    const integer1 = target - array[i];
+    for (let j = i + 1; j <= array.length; j++){
+      if (integer1 === array[j]) return true 
+    }
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +15,16 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  integer1 = target minus integer 2
+  if integer1 = and integer in the array,
+  return true else false
 */
 
 /*
   Add written explanation of your solution here
+
+  I need to find two integers from an array whose sum equal a given target. If the two numbers are available in the array, return true.
+  if not, return false. 
 */
 
 // You can run `node index.js` to view these console logs
